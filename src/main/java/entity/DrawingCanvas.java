6d901982b47476aac20aabea2f3dcbd1a7233388
ecorbin567 <ecorbin567@gmail.com>
2025-07-15 17:47:57 -0400

    fixed draw and erase

--- src/main/java/entity/DrawingCanvas.java
+++ src/main/java/entity/DrawingCanvas.java
@@ -8,10 +8,12 @@ import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 public class DrawingCanvas extends JPanel implements MouseListener, MouseMotionListener {
-    private ArrayList<Point> points = new ArrayList<>();
     private Paintbrush paintbrush;
     private Eraser eraser;
     private Color backgroundColor;
+    private final ArrayList<StrokeRecord> strokes = new ArrayList<>();
+    private StrokeRecord currentStroke;
+
     public DrawingCanvas() {
         setBackground(Color.WHITE);
         this.backgroundColor = Color.WHITE;
@@ -24,40 +26,50 @@ public class DrawingCanvas extends JPanel implements MouseListener, MouseMotionL
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
 
-        Graphics2D g2d = (Graphics2D) g;
-        if (this.eraser.isErasing()) {
-            float width = this.eraser.getWidth();
-            BasicStroke stroke = new BasicStroke(width);
-            g2d.setStroke(stroke);
-            g2d.setColor(backgroundColor);
-        }
-        else {
-            float width = this.paintbrush.getWidth();
-            BasicStroke stroke = new BasicStroke(width);
-            g2d.setStroke(stroke);
-            g2d.setColor(this.paintbrush.getColour());
-        }
-        for (int i = 1; i < points.size(); i++) {
-            Point p1 = points.get(i - 1);
-            Point p2 = points.get(i);
-            if (p1 != null && p2 != null) {
-                g2d.drawLine(p1.x, p1.y, p2.x, p2.y);
+        for (StrokeRecord s : strokes) {
+            g2.setColor(s.colour);
+            g2.setStroke(new BasicStroke(s.width,
+                    BasicStroke.CAP_ROUND,
+                    BasicStroke.JOIN_ROUND));
+            for (int i = 1; i < s.pts.size(); i++) {
+                Point p1 = s.pts.get(i - 1);
+                Point p2 = s.pts.get(i);
+                g2.drawLine(p1.x, p1.y, p2.x, p2.y);
             }
         }
     }
 
+
     @Override
     public void mousePressed(MouseEvent e) {
-        points.add(e.getPoint());  // start a new stroke
-        repaint();
+        if (SwingUtilities.isLeftMouseButton(e)) {
+            Color c = eraser.isErasing() ? backgroundColor
+                    : paintbrush.getColour();
+            float w = eraser.isErasing() ? eraser.getWidth()
+                    : paintbrush.getWidth();
+
+            currentStroke = new StrokeRecord(c, w);
+            currentStroke.pts.add(e.getPoint());
+            strokes.add(currentStroke);
+        }
     }
+
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        points.add(e.getPoint());  // keep adding points as we drag
-        repaint();
+        if (currentStroke != null) {
+            currentStroke.pts.add(e.getPoint());
+            repaint();                 // ask Swing to invoke paintComponent()
+        }
     }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        currentStroke = null;          // finished; ready for a fresh stroke
+    }
+
 
     public void erase() {
         this.eraser.setErasing(true);
@@ -68,7 +80,6 @@ public class DrawingCanvas extends JPanel implements MouseListener, MouseMotionL
     }
 
     // We don't need these, but must include them:
-    @Override public void mouseReleased(MouseEvent e) { points.add(null); }
     @Override public void mouseClicked(MouseEvent e) {}
     @Override public void mouseEntered(MouseEvent e) {}
     @Override public void mouseExited(MouseEvent e) {}
