beef33c42caa9b8f47e1ee9f5d8b600f5cc80e6d
ecorbin567 <ecorbin567@gmail.com>
2025-07-15 16:45:35 -0400

    implement basic drawing capabilities

--- src/main/java/entity/DrawingCanvas.java
+++ src/main/java/entity/DrawingCanvas.java
@@ -1,4 +1,51 @@
 package entity;
 
-public class DrawingCanvas {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+
+public class DrawingCanvas extends JPanel implements MouseListener, MouseMotionListener {
+    private ArrayList<Point> points = new ArrayList<>();
+
+    public DrawingCanvas() {
+        setBackground(Color.WHITE);
+        addMouseListener(this);
+        addMouseMotionListener(this);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLACK);
+        for (int i = 1; i < points.size(); i++) {
+            Point p1 = points.get(i - 1);
+            Point p2 = points.get(i);
+            if (p1 != null && p2 != null) {
+                g.drawLine(p1.x, p1.y, p2.x, p2.y);
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        points.add(e.getPoint());  // start a new stroke
+        repaint();
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        points.add(e.getPoint());  // keep adding points as we drag
+        repaint();
+    }
+
+    // We don't need these, but must include them:
+    @Override public void mouseReleased(MouseEvent e) { points.add(null); }
+    @Override public void mouseClicked(MouseEvent e) {}
+    @Override public void mouseEntered(MouseEvent e) {}
+    @Override public void mouseExited(MouseEvent e) {}
+    @Override public void mouseMoved(MouseEvent e) {}
 }
--- src/main/java/entity/Eraser.java
+++ src/main/java/entity/Eraser.java
@@ -1,4 +1,23 @@
 package entity;
 
 public class Eraser implements DrawableElement{
+
+    private double width;
+
+    public Eraser(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    @Override
+    public void draw(DrawingCanvas drawingCanvas) {
+//        TODO: implement draw
+    }
 }
