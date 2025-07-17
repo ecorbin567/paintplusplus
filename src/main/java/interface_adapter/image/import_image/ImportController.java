f10c1775425175d4299f75c753d1e0ca7cb802be
Lauren Park <chanyi.park@mail.utoronto.ca>
2025-07-17 19:37:16 -0400

    image

--- src/main/java/interface_adapter/image/import_image/ImportController.java
+++ src/main/java/interface_adapter/image/import_image/ImportController.java
@@ -1,4 +1,14 @@
 package interface_adapter.image.import_image;
 
+import use_case.image.import_image.ImportInputBoundary;
+
 public class ImportController {
+    private final ImportInputBoundary interactor;
+
+    public ImportController(ImportInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    public void execute() {}
+
 }
