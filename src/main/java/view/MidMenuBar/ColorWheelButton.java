69496509f722020c39fa4dedeef7dca816841482
Victor Jiang <victorjchess9@gmail.com>
2025-07-19 15:00:28 -0400

    added colorwheel

--- src/main/java/view/MidMenuBar/ColorWheelButton.java
+++ src/main/java/view/MidMenuBar/ColorWheelButton.java
@@ -1,4 +1,23 @@
 package view.MidMenuBar;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class ColorWheelButton {
+    private final JButton button;
+    private final ImageIcon imageIcon;
+
+    public ColorWheelButton(){
+        button = new JButton();
+        imageIcon = new ImageIcon();
+
+
+
+    }
+
+    public JButton getButton(){
+        return this.button;
+    }
 }
