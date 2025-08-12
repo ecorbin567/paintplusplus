5ce402eb68c5bb112b5e67375384e0333fba3e8f
Victor Jiang <victorjchess9@gmail.com>
2025-08-12 12:26:37 -0400

    added selection test package and selection interactor test file, will add more later

---
+++ src/test/java/use_case/selection/SelectionInteractorTest.java
@@ -0,0 +1,30 @@
+package use_case.selection;
+
+import interface_adapter.ViewManagerModel;
+import interface_adapter.canvas.CanvasViewModel;
+import interface_adapter.newselection.SelectionViewModel;
+import interface_adapter.newselection.SelectionPresenter;
+import org.junit.jupiter.api.Test;
+import use_case.newselection.*;
+
+public class SelectionInteractorTest {
+
+    final SelectionViewModel selectionViewModel = new SelectionViewModel();
+    final CanvasViewModel canvasViewModel = new CanvasViewModel();
+
+    public SelectionInteractorTest(){
+
+    }
+    @Test
+    public void testNewValidSelection(){
+
+    }
+    @Test
+    public void testSelectedContentsIsRendering(){
+
+    }
+    @Test
+    public void originalSelectedRegionIsWhitedOut(){
+
+    }
+}
