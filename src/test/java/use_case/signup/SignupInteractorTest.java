8051e5d6ebe95177ebe610178091d974d6302435
josh <joshdrvillas@gmail.com>
2025-08-13 18:34:55 -0400

    recreate login, signup files

--- src/test/java/use_case/signup/SignupInteractorTest.java
+++ src/test/java/use_case/signup/SignupInteractorTest.java
@@ -1,11 +1,10 @@
-package use_case.userdatabase;
+package use_case.signup;
 
 import data_access.InMemoryUserDataAccessObject;
 import entity.CommonUserFactory;
 import entity.User;
 import entity.UserFactory;
 import org.junit.jupiter.api.Test;
-import use_case.signup.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
